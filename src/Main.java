07a22270dbfca48aaa946a6f87c5d5d5dfd77da0
Artur Vargas <arturvargaslg@gmail.com>
2025-03-31 23:08:48 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        *  Collatz Conjecture:
+            1) Ask the user for an integer.
+            2) Apply the conjecture formula until the number is 1.
+           - If the number is even, divide by 2.
+           - If the number is odd, multiply it by 3 and add 1
+           - Print all iterations
+           - Stop the program only when the result is 1
+        *
+        * */
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type an integer: ");
+        int m = scan.nextInt();
+
+        while(m != 1) {
+            if(m % 2 == 0) {
+                m = m/2;
+            } else  {
+                m = (m*3) + 1;
+            }
+            System.out.print(m + "->");
+        }
+
+    }
+}
